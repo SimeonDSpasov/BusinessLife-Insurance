f87d549a6766b58b3507d4670f39254fe84dd429
Simeon Spasov <47272153+PotatoKingBG@users.noreply.github.com>
2021-10-23 13:31:48 +0300

    enter

---
+++ Eurolife1/contact-form-handler.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_Post['name'];
+    $visitor_email = $_Post['email']
+    $message = $_Post['message'];
+
+    $email_from = 'thegamefan36@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User name: $name. \n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+                        
+    $to = "thegamefan36@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: kontakti.html");
+?>
