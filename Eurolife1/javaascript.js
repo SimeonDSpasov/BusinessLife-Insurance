730eb2fcc279833f53828c8deac4a141c76df40b
Simeon Spasov <47272153+PotatoKingBG@users.noreply.github.com>
2021-12-12 01:18:19 +0200

    commit

--- Eurolife1/javaascript.js
+++ Eurolife1/javaascript.js
@@ -19,31 +19,3 @@ function myFunction() {
   console.log(text);
 
 
-// Define selector for selecting
-        // anchor links with the hash
-        let anchorSelector = 'a[href^="biznes.html"]';
-     
-        $(anchorSelector).on('click', function (e) {
-         
-            // Prevent scrolling if the
-            // hash value is blank
-            e.preventDefault();
-     
-            // Get the destination to scroll to
-            // using the hash property
-            let destination = $(this.hash);
-     
-            // Get the position of the destination
-            // using the coordinates returned by
-            // offset() method
-            let scrollPosition
-                = destination.offset().top;
-     
-            // Specify animation duration
-            let animationDuration = 500;
-     
-            // Animate the html/body with
-            // the scrollTop() method
-            $('html, body').animate({
-                scrollTop: scrollPosition
-            }, animationDuration);
